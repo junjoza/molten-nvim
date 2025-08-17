7e0787fc8ebf7e46a507e5a0b3b0065a06fd156a
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2025-08-17 01:37:42 -0600

    From v1.9.2
    Signed-off-by: yo-lo-pregunto <junjo.mendoza11@gmail.com>

--- rplugin/python3/molten/options.py
+++ rplugin/python3/molten/options.py
@@ -17,6 +17,7 @@ class HL:
     foot = "MoltenOutputFooter"
     cell = "MoltenCell"
     virtual_text = "MoltenVirtualText"
+    virtual_status = "MoltenVirtualStatus"
 
     defaults = {
         border_norm: "FloatBorder",
@@ -27,6 +28,7 @@ class HL:
         foot: "FloatFooter",
         cell: "CursorLine",
         virtual_text: "Comment",
+        virtual_status: "Operator"
     }
 
 
@@ -64,6 +66,7 @@ class MoltenOptions:
     virt_text_max_lines: int
     virt_text_output: bool
     virt_text_truncate: Literal["top", "bottom"]
+    virt_status_on_header: bool
     wrap_output: bool
     nvim: Nvim
     hl: HL
@@ -109,6 +112,7 @@ class MoltenOptions:
             ("molten_output_win_zindex", 50),
             ("molten_virt_text_truncate", "bottom"),
             ("molten_floating_window_focus", "top"),
+            ("molten_virt_status_on_header", False),
         ]
         # fmt: on
 
--- rplugin/python3/molten/outputbuffer.py
+++ rplugin/python3/molten/outputbuffer.py
@@ -35,6 +35,7 @@ class OutputBuffer:
     display_virt_lines: Optional[DynamicPosition]
     extmark_namespace: int
     virt_text_id: Optional[int]
+    virt_text_status_id: Optional[int]
     displayed_status: OutputStatus
 
     options: MoltenOptions
@@ -52,6 +53,7 @@ class OutputBuffer:
         self.virt_hidden: bool = False
         self.extmark_namespace = extmark_namespace
         self.virt_text_id = None
+        self.virt_text_status_id = None
         self.displayed_status = OutputStatus.HOLD
 
         self.options = options
@@ -78,15 +80,20 @@ class OutputBuffer:
         match output.status:
             case OutputStatus.HOLD:
                 status = "* On Hold"
+                output.hl = self.options.hl.border_norm
             case OutputStatus.DONE:
                 if output.success:
-                    status = "✓ Done"
+                    status = "✓"
+                    output.hl = self.options.hl.border_succ
                 else:
-                    status = "✗ Failed"
+                    status = "✗"
+                    output.hl = self.options.hl.border_fail
             case OutputStatus.RUNNING:
-                status = "... Running"
+                status = "↻"
+                output.hl = self.options.hl.border_norm
             case OutputStatus.NEW:
                 status = ""
+                output.hl = self.options.hl.border_succ
             case _:
                 raise ValueError("bad output.status: %s" % output.status)
 
@@ -171,6 +178,8 @@ class OutputBuffer:
             # self.displayed_status = OutputStatus.NEW
             self.virt_hidden = True
 
+        if self.virt_text_status_id is not None:
+            self.nvim.funcs.nvim_buf_del_extmark(bufnr, self.extmark_namespace, self.virt_text_status_id)
         # clear any inline images, etc.
         redraw = False
         for chunk in self.output.chunks:
@@ -244,6 +253,33 @@ class OutputBuffer:
 
         lines.insert(0, self._get_header_text(self.output))
         return lines, len(lines) - 1 + virtual_lines
+
+    def show_status_on_header(self, anchor: Position) -> None:
+        if self.displayed_status == OutputStatus.DONE and self.virt_text_status_id is not None:
+            return
+
+        buf = self.nvim.buffers[anchor.bufno]
+
+        if self.virt_text_status_id is not None:
+            self.nvim.funcs.nvim_buf_del_extmark(
+                anchor.bufno, self.extmark_namespace, self.virt_text_status_id
+            )
+            self.virt_text_status_id = None
+
+        text = f"{self._get_header_text(self.output)}"
+        self.virt_text_status_id = buf.api.set_extmark(
+            self.extmark_namespace,
+            anchor.lineno,
+            0,
+            {
+                "virt_text": [[text, self.output.hl]],
+                "virt_text_pos": "inline",
+                "hl_mode": "combine",
+                "virt_text_win_col": 100 - len(text),
+            }
+        )
+        self.canvas.present()
+
 
     def show_virtual_output(self, anchor: Position) -> None:
         if self.virt_hidden:
